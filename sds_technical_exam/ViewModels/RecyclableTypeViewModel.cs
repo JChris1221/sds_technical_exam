2dda9547f777b139ccbece207bfad6151e987e7a
JChris1221 <jcpingkian@gmail.com>
2023-06-17 03:17:15 +0800

    Create viewmodel for recyclable type and add navbar partial view

---
+++ sds_technical_exam/ViewModels/RecyclableTypeViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace sds_technical_exam.ViewModels
+{
+    public class RecyclableTypeViewModel
+    {
+        Models.RecyclableType RecyclableType { get; set; }
+    }
+}
